6f7e16c81307e48fb497740143cdc23bf250d10d
Parvej Mallick <parvej.mallick@cbnits.com>
2019-12-10 14:13:58 +0530

    route is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Page404 from './components/NotFound/404page';
+import SignUp from './components/Login/SignUp';
+import Login from './components/Login/login';
+import Home from './components/HomePage/home';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-        <h1>Hello World</h1>
-    </div>
-  );
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+          
+        {/* Creating Router */}
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/signup">SignUp</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+            <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/login" component={Login} />
+            <Route component={Page404} />
+            </Switch>
+          </div>
+        </Router>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
